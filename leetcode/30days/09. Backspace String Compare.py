e6931092178237a0e9c06f980b1a529752e657df
Axel Prieto <prietoaxel@gmail.com>
2020-04-10 13:21:54 +0200

    Daily programming practice

---
+++ leetcode/30days/09. Backspace String Compare.py
@@ -0,0 +1,12 @@
+class Solution:
+    def backspaceCompare(self, S: str, T: str) -> bool:
+        def finalStr(s: str):
+            stack = []
+            for char in s:
+                if stack and char == '#':
+                    stack.pop()
+                elif char != '#':
+                    stack.append(char)
+            return stack
+
+        return finalStr(S) == finalStr(T)
