f4541f80287585a140c73d802b011470bb907868
Axel Prieto <prietoaxel@gmail.com>
2021-12-05 11:17:50 +0000

    minor changes

---
+++ hackerrank/python/easy/python-loops.py
@@ -0,0 +1,9 @@
+if __name__ == '__main__':
+    n = int(input())
+
+    counter = 0
+    add_amount = 1
+    for i in range(1, n + 1):
+        print(counter)
+        counter += add_amount
+        add_amount += 2
