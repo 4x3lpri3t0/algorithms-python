55627374c5a1e7a8aca5ce01880eb6577c64fd08
Axel Prieto <prietoaxel@gmail.com>
2021-12-13 20:21:48 +0000

    minor changes

---
+++ hackerrank/python/easy/zipped.py
@@ -0,0 +1,8 @@
+N, X = map(int, input().split())
+grades_by_subject = []
+
+for _ in range(X):
+    grades_by_subject.append(map(float, input().split()))
+
+for grades_by_student in zip(*grades_by_subject):
+    print(sum(grades_by_student) / len(grades_by_student))
